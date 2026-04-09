42a5e448ebbb0a786626ed61a37cb3d7acacda27
rajalingarao4545 <rajalingarao4545@gmail.com>
2026-04-09 15:43:20 +0530

    terraform aws vpc

---
+++ outputs.tf
@@ -0,0 +1,22 @@
+output "vpc_id" {
+    value = aws_vpc.main.id
+}
+output "igw_id" {
+    value = aws_internet_gateway.gw.id
+}
+output "public_subnet_ids" {
+    value = aws_subnet.public[*].id
+}
+output "private_subnet_ids" {
+    value = aws_subnet.private[*].id
+}
+output "database_subnet_ids" {
+    value = aws_subnet.database[*].id
+}
+
+output "database_subnet_group_id" {
+    value = aws_db_subnet_group.default.id
+}
+output "database_subnet_group_name" {
+    value = aws_db_subnet_group.default.name
+}
