4bda78fbadb63e406047bb224597c28fb50b7cdb
rajalingarao4545 <rajalingarao4545@gmail.com>
2026-04-09 17:45:15 +0530

    terraform aws vpc

--- data.tf
+++ data.tf
@@ -1,3 +1,14 @@
 data "aws_availability_zones" "available" {
     state = "available"
 }
+ # This is very important aws_database_group_name
+data "aws_vpc" "default" {
+    default = true
+}
+data "aws_route_table" "main" {
+    vpc_id = data.aws_vpc.default.id
+    filter {
+      name = "association.main"
+      values = ["true"]
+    }
+}
